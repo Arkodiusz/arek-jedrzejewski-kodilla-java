38862a67410e6c9df17934f2dc7cdb3f2fecebd6
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2020-10-12 20:07:06 +0200

    master

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+//        import com.kodilla.testing.user.SimpleUser;
+        import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        Calculator calculator = new Calculator();
+
+        if (calculator.add(a, b) == a + b) {
+            System.out.println("Add - test OK");
+        } else {
+            System.out.println("Add - Error!");
+        }
+
+        if (calculator.sub(a, b) == a - b) {
+            System.out.println("Sub - test OK");
+        } else {
+            System.out.println("Sub - Error!");
+        }
     }
 }
+
+
+//        SimpleUser simpleUser = new SimpleUser("theForumUser");
+//
+//        String result = simpleUser.getUsername();
+//
+//        if (result.equals("theForumUser")){
+//            System.out.println("Simple user - test OK");
+//        } else {
+//            System.out.println("Simple user - Error!");
+//        }
