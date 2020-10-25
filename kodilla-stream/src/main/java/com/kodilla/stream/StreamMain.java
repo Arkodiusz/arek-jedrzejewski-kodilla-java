7020def85185212273081036a4597e410d94a8e7
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2020-10-25 22:09:58 +0100

    7.2 zad  poprawione

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,77 +1,21 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
-//import com.kodilla.stream.lambda.ExpressionExecutor;
-//import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
 
-import static java.lang.String.valueOf;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamMain {
+
     public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        String text1 = "lockdown";
-        String text2 = "kodilla";
-        String text3 = "vertical";
-        String text4 = "conversion to camel case";
-
-        poemBeautifier.beautify(text1, String::toUpperCase);
-
-        poemBeautifier.beautify(text2, (ugly) -> {
-            String beautiful = ugly.replace('a', '@');
-            beautiful = beautiful.replace('o', '0');
-            return "\n" + beautiful;
-        });
-
-        poemBeautifier.beautify(text3, (ugly) -> {
-            char[] textArray = ugly.toCharArray();
-            StringBuilder beautiful = new StringBuilder("\n");
-            for (char c : textArray) {
-                beautiful.append(c).append("\n");
-            }
-            return beautiful.toString();
-        });
-
-        poemBeautifier.beautify(text4, (ugly) -> {
-            char[] textArray = ugly.toCharArray();
-            StringBuilder beautiful = new StringBuilder();
-
-            for (int i = 0; i < textArray.length; i++) {
-                if (textArray[i] == ' ') {
-                    textArray[i + 1] = textArray[i+1];
-                    String c = valueOf(textArray[i+1]);
-                    c = c.toUpperCase();
-                    textArray[i+1]=c.charAt(0);
-                }
-            }
-
-            for (char value : textArray) {
-                if (value != ' ') {
-                    beautiful.append(value);
-                }
-            }
-
-            return beautiful.toString();
-        });
-
-//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-//
-//        System.out.println("Calculating expressions with lambdas");
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-//        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-//
-//        System.out.println("Calculating expressions with method references");
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-//        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
-        System.out.println("\nUsing Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
     }
 }
