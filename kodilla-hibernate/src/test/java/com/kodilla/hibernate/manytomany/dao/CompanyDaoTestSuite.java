eb771b308750d9730fb172c93ad7bdd41df73d7a
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-01-12 19:38:30 +0100

    17_4_zadanie_HQL

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -12,6 +14,8 @@ class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany() {
@@ -56,6 +60,70 @@ class CompanyDaoTestSuite {
             companyDao.deleteById(greyMatterId);
         } catch (Exception e) {
             //do nothing
+        }
+    }
+
+    @Test
+    void testEmployeeRetrieveByLastname() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        //When
+        employeeDao.save(johnSmith);
+        int johnSmithId = johnSmith.getId();
+        employeeDao.save(stephanieClarckson);
+        int stephanieClarcksonId = stephanieClarckson.getId();
+        employeeDao.save(lindaKovalsky);
+        int lindaKovalskyId = lindaKovalsky.getId();
+
+        List<Employee> results1 = employeeDao.retrieveByLastname("Kovalsky");
+        List<Employee> results2 = employeeDao.retrieveByLastname("Nowack");
+
+        //Then
+        try {
+            assertEquals(1, results1.size());
+            assertEquals(0, results2.size());
+
+        } finally {
+            //Cleanup
+            employeeDao.deleteById(johnSmithId);
+            employeeDao.deleteById(stephanieClarcksonId);
+            employeeDao.deleteById(lindaKovalskyId);
+        }
+    }
+
+    @Test
+    void testCompanyRetrieveByNameBegin() {
+        //Given
+        Company softwareMachine = new Company("Software Machine");
+        Company softwareMasters = new Company("Software Masters");
+        Company greyMatter = new Company("Grey Matter");
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+        companyDao.save(softwareMasters);
+        int softwareMastersId = softwareMasters.getId();
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        List<Company> result1 = companyDao.retrieveByNameBegin("Sof");
+        List<Company> result2 = companyDao.retrieveByNameBegin("Gre");
+        List<Company> result3 = companyDao.retrieveByNameBegin("Abc");
+
+        //Then
+        try {
+            assertEquals(2, result1.size());
+            assertEquals(1, result2.size());
+            assertEquals(0, result3.size());
+
+        } finally {
+            //Cleanup
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(softwareMastersId);
+            companyDao.deleteById(greyMatterId);
         }
     }
 }
