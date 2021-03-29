c78e7aa2e32e9ebf85b0570f056262cdbfb9d49e
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-03-29 21:43:32 +0200

    24_1 environment prepared for patterns2 module

---
+++ src/test/java/com/kodilla/KodillaApplicationTest.java
@@ -0,0 +1,15 @@
+package com.kodilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaApplicationTest {
+
+
+    @Test
+    void contextLoad() {
+
+
+    }
+}
