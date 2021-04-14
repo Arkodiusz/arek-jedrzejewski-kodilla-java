a245025e97b52a1ab00cb927db6cdb59fe978545
Arkodiusz <arecki358@gmail.com>
2021-04-14 21:07:36 +0200

    31_2 task - selenium facebook tests xpath reduced

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -10,15 +10,15 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class FacebookTestingApp {
 
     public static final String XPATH_BUTTON_ACCEPT_COOKIES = "//button[@data-testid='cookie-policy-dialog-accept-button']";
-    public static final String XPATH_BUTTON_NEW_ACCOUNT = "//div[@class='_6ltg']/a[@data-testid='open-registration-form-button']";
-    public static final String XPATH_INPUT_NAME =  "//div[@class='_n8 _3qx _8idq _8esf _8f3m _8fgk uiLayer _3qw']/div/div/div/div/div/div/div/form/div/div/div/div/div/input[@name='firstname']";
+    public static final String XPATH_BUTTON_NEW_ACCOUNT = "//a[@data-testid='open-registration-form-button']";
+    public static final String XPATH_INPUT_NAME =  "//input[@name='firstname']";
     public static final String XPATH_INPUT_SURNAME =  "//input[@name='lastname']";
     public static final String XPATH_INPUT_CONTACT =  "//input[@name='reg_email__']";
     public static final String XPATH_INPUT_PASSWORD =  "//input[@name='reg_passwd__']";
     public static final String XPATH_SELECT_DAY =  "//select[@name='birthday_day']";
     public static final String XPATH_SELECT_MONTH =  "//select[@name='birthday_month']";
     public static final String XPATH_SELECT_YEAR =  "//select[@name='birthday_year']";
-    public static final String XPATH_SELECT_MALE =  "//div[@class='_n8 _3qx _8idq _8esf _8f3m _8fgk uiLayer _3qw']/div/div/div/div/div/div/div/form/div/div/span/span/input[@value='2']";
+    public static final String XPATH_SELECT_MALE =  "//input[@value='2']";
 
     public static void main(String[] args) {
 
